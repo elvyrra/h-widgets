280496e2424f00c72877dac16401dd70ff8a4c7e
seb <lecocq.elvyrra@gmail.com>
2016-11-28 13:58:02 +0100

    possibility to set avatar or name in mleta data widget

--- widgets/MetaData.php
+++ widgets/MetaData.php
@@ -4,16 +4,26 @@ namespace Hawk\Plugins\HWidgets;
 
 class MetaData extends Widget {
     public function display() {
-        $user = User::getById($this->userId);
+        if(isset($this->avatar)) {
+            $avatar = $this->avatar;
+        }
+        elseif(isset($this->name)) {
+            $avatar = '';
+            $initial = strtoupper(substr($this->name, 0, 1));
+            $bgColor = Utils::strToColor($this->name);
+        }
+        else {
+            $user = User::getById($this->userId);
+            $avatar = $user->getProfileData('avatar');
+            $initial = strtoupper(substr($user->username, 0, 1));
+
+            $bgColor = Utils::strToColor($user->username);
+        }
 
         $size = !empty($this->size) ? $this->size : '';
         $meta = !empty($this->meta) ? $this->meta : '';
         $description = !empty($this->description) ? $this->description : '';
 
-        $avatar = $user->getProfileData('avatar');
-        $initial = strtoupper(substr($user->username, 0, 1));
-
-        $bgColor = Utils::strToColor($user->username);
         $r = hexdec(substr($bgColor, 0, 2));
         $g = hexdec(substr($bgColor, 2, 2));
         $b = hexdec(substr($bgColor, 4, 2));
